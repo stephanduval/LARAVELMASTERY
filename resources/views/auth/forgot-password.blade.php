6d7e7f2478b450138381f72604cd24b504411045
Stephan DuVal <stephan.duval@gmail.com>
2023-08-10 11:40:39 -0700

    finished leeson 4-41

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,13 +5,20 @@
         <div class="col-md-12">
             <div class="card">
                 <h3>
-                    Login to Task Tracking MIS
+                    Reset your password
                 </h3>
+
+                @if (session('status'))
+                    <div class="text-success">
+                        {{ session('status') }}
+
+                    </div>
+                @endif
                 <div class="card-header bg-secondary">
-                    <h5 class="text-center text-light">Login</h5>
+                    <h5 class="text-center text-light">Reset Password</h5>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('login') }}" method="POST">
+                    <form action="{{ route('password.email') }}" method="POST">
                         @csrf
                         <div class="form-group my-1">
                             <label for="email">Email</label>
@@ -24,36 +31,16 @@
                                 </span>
                             @enderror
                         </div>
-                        <div class="form-group
-                                my-1">
-                            <label for="password">Password</label>
-                            <input type="password" name="password"
-                                class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}"
-                                placeholder="Password" autocomplete="new-password" required>
 
-                            @error('password')
-                                <span class="invalid-feedback">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form my-1">
-                                    <a href="{{ route('password.request') }}">Forgot Password</a>
-                                </div>
-                            </div>
-                        </div>
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group my-1">
-                                    <button type="submit" class="btn btn-warning float-start">Login</button>
+                                    <button type="submit" class="btn btn-warning float-start">Reset Link</button>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group my-1">
-                                    <a href="{{ route('register') }}" class="btn btn-dark float-end">Register</a>
+                                    <a href="{{ route('login') }}" class="btn btn-dark float-end">Login</a>
                                 </div>
                             </div>
                         </div>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,14 +5,17 @@
         <div class="col-md-12">
             <div class="card">
                 <h3>
-                    Login to Task Tracking MIS
+                    Reset Password
                 </h3>
                 <div class="card-header bg-secondary">
-                    <h5 class="text-center text-light">Login</h5>
+                    <h5 class="text-center text-light">Register</h5>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('login') }}" method="POST">
+                    <form action="{{ url('reset-password') }}" method="POST">
                         @csrf
+
+                        <input type="hidden" name="token" value="{{ $request->token }}">
+
                         <div class="form-group my-1">
                             <label for="email">Email</label>
                             <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
@@ -24,9 +27,9 @@
                                 </span>
                             @enderror
                         </div>
-                        <div class="form-group
-                                my-1">
-                            <label for="password">Password</label>
+
+                        <div class="form-group my-1">
+                            <label for="password">Reset Password</label>
                             <input type="password" name="password"
                                 class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}"
                                 placeholder="Password" autocomplete="new-password" required>
@@ -37,25 +40,24 @@
                                 </span>
                             @enderror
                         </div>
+                        <div class="form-group my-1">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password" name="password_confirmation"
+                                class="form-control autocomplete="new-password" required>
 
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form my-1">
-                                    <a href="{{ route('password.request') }}">Forgot Password</a>
-                                </div>
-                            </div>
+                            @error('password')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group my-1">
-                                    <button type="submit" class="btn btn-warning float-start">Login</button>
+                                    <button type="submit" class="btn btn-warning float-start">Register</button>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group my-1">
-                                    <a href="{{ route('register') }}" class="btn btn-dark float-end">Register</a>
-                                </div>
-                            </div>
+
                         </div>
                     </form>
                 </div>
